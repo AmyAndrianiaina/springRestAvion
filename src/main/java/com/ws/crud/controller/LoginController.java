a215257eb33c269c33597b32ed295a5b4e093e24
AmyAndrianiaina <99749134+AmyAndrianiaina@users.noreply.github.com>
2022-12-20 17:02:28 +0300

    Update LoginController.java

--- src/main/java/com/ws/crud/controller/LoginController.java
+++ src/main/java/com/ws/crud/controller/LoginController.java
@@ -20,7 +20,7 @@ import com.ws.crud.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-@CrossOrigin(origins = { "http://localhost", "http://localhost:3000", "https://springrestavion-production.up.railway.app" })
+@CrossOrigin(origins = { "http://localhost", "http://localhost:3000", "https://springrestavion-production.up.railway.app/user",  "https://springrestavion-production.up.railway.app/avions" })
 @RestController
 public class LoginController {
 	
